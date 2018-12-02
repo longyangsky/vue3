562a0067959a755ee5318e1eff13d8a672a28c83
longyang <longyangyxm@126.com>
2018-12-03 00:54:04 +0800

    '测试'

---
+++ nodeAPI/config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb://test:Sky1986@47.101.137.218/test'
+}
---
+++ nodeAPI/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+const UserSchema = new Schema({
+    name :{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    avator:{
+        type:String
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    createdate:{
+        type:Date,
+        default:Date.now
+    }
+
+});
+
+module.exports =mongoose.model("users",UserSchema);
