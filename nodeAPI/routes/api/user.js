1bfd21479a76b01e4c6d5706e67a56236aea22b6
longyangsky <longyangyxm@126.com>
2018-12-03 17:24:26 +0800

    登录与注册接口实现

--- nodeAPI/routes/api/user.js
+++ nodeAPI/routes/api/user.js
@@ -4,30 +4,50 @@ const User = require('../../models/User');
 const bcrypt= require("bcrypt")
 
 router.post("/register",(req,res)=>{  
-    User.findOne({email:req.body.email}).then(aa=>{
-        console.log('sss')
-        if(aa){ 
-            res.status(400).json({err:"已被注册"});
+    User.findOne({email:req.body.email}).then(user=>{  
+        if(user){ 
+            return res.status(400).json({err:"已被注册"});
         }else{
             const newUser = new User({
                 name:req.body.name, 
-                email:'11',
-            }) 
-            bcrypt.genSalt(10, function(err, salt) {
-                console.log(err)
-                /*bcrypt.hash(req.body.passwrod, salt, (err, hash)=> {
-                     if(err){throw err};
-                     Usersxx.password =  hash; 
+                password:req.body.password,
+                email:req.body.email
+            })  
+            bcrypt.genSalt(10, function(err, salt) { 
+                bcrypt.hash(newUser.password, salt, (err, hash)=> { 
+                     if(err){
+                         res.json(err)
+                     };
+                     newUser.password =  hash; 
                      newUser.save().then(user=>{
-                         res.json(user);
+                           res.json(user); 
                      })
                      .catch(err=>res.json('发生错误'))
-                });*/
+                });
             });
 
         }
     }) 
 })
- 
+
+router.post("/login",(req,res)=>{
+    const  email = req.body.email;
+    const password = req.body.password; 
+    User.findOne({email:email})
+    .then((user)=>{ 
+        if(!user){
+         res.status(404).json({user:"用户不存在"});
+        }else{ 
+            bcrypt.compare(password, user.password)
+            .then(ismatch=>{ 
+                    if(ismatch){
+                        res.json('匹配正确');
+                    }else{
+                        res.json('密码错误');
+                    }
+            })
+        }
+    })
+})
 
 module.exports = router;
